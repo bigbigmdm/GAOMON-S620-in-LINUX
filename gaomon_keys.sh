89c8c49c0d5c2b3294107582c2e05046467d8a2d
Mikhail Medvedev <75389103+bigbigmdm@users.noreply.github.com>
2022-11-09 09:53:30 +0300

    Update gaomon_keys.sh
    Errors deleting

--- gaomon_keys.sh
+++ gaomon_keys.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # TABLET KEY SETTINGS GAOMON S620
-xsetwacom --set 'GAOMON Gaomon Tablet Pad Pad' Button 1 "key +ctrl c -ctrl"
-xsetwacom --set 'GAOMON Gaomon Tablet Pad' Button 2 "key +ctrl v -ctrl"
-xsetwacom --set 'GAOMON Gaomon Tablet Pad Pad' Button 3 "key +ctrl - -ctrl"
-xsetwacom --set 'GAOMON Gaomon Tablet Pad Pad' Button 8 "key +ctrl + -ctrl"
+xsetwacom --set 'GAOMON Gaomon Tablet Pad pad' Button 1 "key +ctrl c -ctrl"
+xsetwacom --set 'GAOMON Gaomon Tablet Pad pad' Button 2 "key +ctrl v -ctrl"
+xsetwacom --set 'GAOMON Gaomon Tablet Pad pad' Button 3 "key +ctrl - -ctrl"
+xsetwacom --set 'GAOMON Gaomon Tablet Pad pad' Button 8 "key +ctrl + -ctrl"
 # GAOMON S620 PEN KEY SETTINGS
 xsetwacom --set 'GAOMON Gaomon Tablet Pen stylus' Button 2 "key del"
